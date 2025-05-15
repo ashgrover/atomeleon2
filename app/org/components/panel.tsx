8a830d8fe4fbad269a7b00ac29658889a2043081
Ash <ashgrover@gmail.com>
2025-05-14 18:29:22 -0600

    Work on sidebar

--- app/org/components/panel.tsx
+++ app/org/components/panel.tsx
@@ -1,38 +1,41 @@
 "use client";
 
-import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarHeader, SidebarMenu, SidebarMenuAction, SidebarMenuButton, SidebarMenuItem, SidebarMenuSub, SidebarMenuSubButton, SidebarMenuSubItem, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { useEffect, useState } from "react";
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+import { Calendar, ChevronDown, File, Home, Inbox, Plus, Settings, SquareChartGantt } from "lucide-react";
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@radix-ui/react-collapsible";
+
+type Project = {
+    title: string,
+    url: string
+}
+type State = {
+    projects: Array<Project>
+}
 
 export default function Panel() {
     // Menu items.
-    const items = [
-        {
-            title: "Home",
-            url: "#",
-            icon: Home,
-        },
-        {
-            title: "Contractors",
-            url: "#",
-            icon: Inbox,
-        },
-        {
-            title: "Timesheets",
-            url: "#",
-            icon: Calendar,
-        },
-        {
-            title: "Projects",
-            url: "#",
-            icon: Search,
-        },
-        {
-            title: "Settings",
-            url: "#",
-            icon: Settings,
-        },
-    ]
+
+    const [state, setState] = useState<State>({ projects: [] });
+
+    useEffect(() => {
+        const projects = [
+            {
+                title: "Project 1",
+                url: "#"
+            },
+            {
+                title: "Project 2",
+                url: "#"
+            },
+            {
+                title: "Project 3",
+                url: "#"
+            },
+        ];
+
+        setState(state => ({ ...state, projects: projects }));
+    }, []);
 
     return (
         <SidebarProvider>
@@ -44,20 +47,89 @@ export default function Panel() {
                     <SidebarGroup>
                         <SidebarGroupContent>
                             <SidebarMenu>
-                                {items.map((item) => (
-                                    <SidebarMenuItem key={item.title}>
+                                <SidebarMenuItem>
+                                    <SidebarMenuButton asChild>
+                                        <a href="#">
+                                            <Home />
+                                            <span>Home</span>
+                                        </a>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+
+                                <SidebarMenuItem>
+                                    <SidebarMenuButton asChild>
+                                        <a href="#">
+                                            <Calendar />
+                                            <span>Timesheets</span>
+                                        </a>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+
+                                <SidebarMenuItem>
+                                    <SidebarMenuButton asChild>
+                                        <a href="#">
+                                            <Inbox />
+                                            <span>Contractors</span>
+                                        </a>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+
+                                <SidebarMenuItem>
+                                    <SidebarMenuButton asChild>
+                                        <a href="#">
+                                            <File />
+                                            <span>Documents</span>
+                                        </a>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+
+                                <Collapsible defaultOpen className="group/collapsible">
+                                    <SidebarMenuItem>
                                         <SidebarMenuButton asChild>
-                                            <a href={item.url}>
-                                                <item.icon />
-                                                <span>{item.title}</span>
-                                            </a>
+                                            <CollapsibleTrigger>
+                                                <SquareChartGantt />
+                                                <span>Projects</span>
+                                                <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+                                            </CollapsibleTrigger>
                                         </SidebarMenuButton>
+                                        <SidebarMenuAction>
+                                            <Plus className="cursor-pointer" />
+                                        </SidebarMenuAction>
+                                        <CollapsibleContent>
+                                            <SidebarMenuSub>
+                                                {state.projects.map(project => (
+                                                    <SidebarMenuSubItem key={project.title}>
+                                                        <SidebarMenuSubButton asChild>
+                                                            <a href="#">
+                                                                <SquareChartGantt />
+                                                                <span>{project.title}</span>
+                                                            </a>
+                                                        </SidebarMenuSubButton>
+                                                    </SidebarMenuSubItem>
+                                                ))}
+                                            </SidebarMenuSub>
+                                        </CollapsibleContent>
                                     </SidebarMenuItem>
-                                ))}
+                                </Collapsible>
+
                             </SidebarMenu>
                         </SidebarGroupContent>
                     </SidebarGroup>
+
                 </SidebarContent>
+                <SidebarFooter>
+                    <SidebarMenu>
+                        <SidebarMenuItem>
+                            <SidebarMenuButton asChild>
+                                <a href="#">
+                                    <Settings />
+                                    <span>Settings</span>
+                                </a>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+
+                    </SidebarMenu>
+                </SidebarFooter>
             </Sidebar>
             <SidebarTrigger />
         </SidebarProvider>
--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -467,7 +467,7 @@ function SidebarMenuItem({ className, ...props }: React.ComponentProps<"li">) {
     <li
       data-slot="sidebar-menu-item"
       data-sidebar="menu-item"
-      className={cn("group/menu-item relative", className)}
+      className={cn("group/menu-item relative mt-2", className)}
       {...props}
     />
   )
@@ -660,7 +660,7 @@ function SidebarMenuSubItem({
     <li
       data-slot="sidebar-menu-sub-item"
       data-sidebar="menu-sub-item"
-      className={cn("group/menu-sub-item relative", className)}
+      className={cn("group/menu-sub-item relative mt-2", className)}
       {...props}
     />
   )
