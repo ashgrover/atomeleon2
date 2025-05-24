195e788b19ad463723a6367c477ea09a7ba20477
Ash <ashgrover@gmail.com>
2025-05-24 15:12:36 -0600

    Work on members

--- app/org/components/memberrow.tsx
+++ app/org/components/memberrow.tsx
@@ -40,7 +40,6 @@ export default function MemberRow({ projectId, member }: { projectId: string, me
                     <TableRow className="hover:bg-transparent">
                         <TableCell colSpan={7} className="pt-2 pb-5">
                             <Tasks className="ml-8" projectId={projectId} contractorId={member.contractorId} />
-                            {/* <UserAssignedTasks member={member} /> */}
                             <div className="my-5" />
                             <Commits commits={mockCodeCommits} />
                         </TableCell>
@@ -50,39 +49,3 @@ export default function MemberRow({ projectId, member }: { projectId: string, me
         </>
     )
 }
-
-// function UserAssignedTasks({ member }: { member: Member }) {
-//     return (
-//         <div className="ml-8 p-3 border-1 rounded-sm bg-neutral-50">
-//             <p className="mb-3 font-bold text-xs">Tasks</p>
-//             <div className=" grid grid-cols-4 gap-y-3 items-center text-sm">
-//                 <p className="font-bold text-slate-500">Task#</p>
-//                 <p className="font-bold text-slate-500">Title</p>
-//                 <p className="font-bold text-slate-500">Status</p>
-//                 <p className="font-bold text-slate-500">Cost</p>
-//                 <p className="font-bold text-slate-500">Created Date</p>
-//                 <div className="col-span-5 border-b-1" />
-
-//                 <a href="" target="_blank" className="text-blue-600 underline">TK-1</a>
-//                 <p>Task 1</p>
-//                 <p><TaskStatusBadge status={TaskStatus.Open} /></p>
-//                 <p>$5,000</p>
-//                 <p>May 20, 2025</p>
-//                 <div className="col-span-5 border-b-1" />
-
-//                 <a href="" target="_blank" className="text-blue-600 underline">TK-1</a>
-//                 <p>Task 1 Task 1</p>
-//                 <p><TaskStatusBadge status={TaskStatus.Open} /></p>
-//                 <p>$5,000</p>
-//                 <p>May 20, 2025</p>
-//                 <div className="col-span-5 border-b-1" />
-
-//                 <a href="" target="_blank" className="text-blue-600 underline">TK-1</a>
-//                 <p>Task 1</p>
-//                 <p><TaskStatusBadge status={TaskStatus.Open} /></p>
-//                 <p>$5,000</p>
-//                 <p>May 20, 2025</p>
-//             </div>
-//         </div>
-//     )
-// }
