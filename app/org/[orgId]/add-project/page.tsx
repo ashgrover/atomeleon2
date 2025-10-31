84c2a6d1f3b093405c9835500339c87ae51157b8
Ash <ashgrover@gmail.com>
2025-10-31 17:33:53 -0600

    Add project, connect repo

--- app/org/[orgId]/add-project/page.tsx
+++ app/org/[orgId]/add-project/page.tsx
@@ -6,7 +6,8 @@ import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { getInstallationId, saveRepo } from "@/lib/database";
 import { createSupabaseBrowserClient } from "@/lib/supabase/client";
-import { use, useEffect, useState } from "react";
+import { Loader2 } from "lucide-react";
+import { FormEvent, use, useEffect, useState } from "react";
 
 type Repo = {
     id: number,
@@ -26,6 +27,82 @@ type RepoState = {
 export default function AddProjectPage({ params }: { params: Promise<{ orgId: string }> }) {
     const { orgId } = use(params);
     const [formState, setFormState] = useState<FormState>({ projName: "", projDesc: "", budget: 0 });
+    const [showIntegrationsState, setShowIntegrationsState] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const onAddProject = async (e: FormEvent) => {
+        e.preventDefault();
+        if (!formState.projName || !formState.projDesc || !formState.budget) return;
+
+        try {
+            setIsLoading(true);
+            const supabase = createSupabaseBrowserClient();
+
+            const result = await supabase.functions.invoke("create-project", {
+                body: {
+                    org_public_id: orgId,
+                    proj_name: formState.projName,
+                    proj_desc: formState.projDesc,
+                    budget: formState.budget
+                }
+            });
+
+            if (result.error) throw result.error;
+
+            setShowIntegrationsState(true);
+
+        } catch (err: unknown) {
+            console.log(err instanceof Error ? err.message : err);
+        }
+
+        setIsLoading(false);
+    }
+    if (showIntegrationsState) {
+        return <ConnectIntegrations orgId={orgId} />
+    }
+
+    return (
+        <div className="bg-slate-50 flex w-full justify-center">
+            <div className="m-8 border-1 rounded-2xl bg-white sm:min-w-3xl max-w-3xl h-fit">
+                <form onSubmit={onAddProject} className="[&>*]:border-b-1 [&>*]:border-gray-200 [&>*]:px-8 [&>*]:py-6">
+                    <div>
+                        <h1 className="text-2xl font-semibold">Add Project</h1>
+                        <p className="text-gray-500 text-sm">You can connect data providers in the next step after you have created the project.</p>
+                    </div>
+                    <div className="grid grid-cols-2">
+                        <Label htmlFor="title-label">Name</Label>
+                        <Input required type="text" id="title-label" aria-labelledby="title-label"
+                            onChange={(e) => setFormState(state => ({ ...state, projName: e.target.value }))} />
+                    </div>
+
+                    <div className="grid grid-cols-2 items-start">
+                        <Label htmlFor="desc-label">Description</Label>
+                        <Textarea id="desc-label" required aria-labelledby="desc-label"
+                            onChange={(e) => setFormState(state => ({ ...state, projDesc: e.target.value }))} />
+                    </div>
+
+                    <div className="grid gap-3 grid-cols-2">
+                        <Label htmlFor="budget-label">Budget</Label>
+                        <Input type="number" id="title-label" required aria-labelledby="budget-label"
+                            onChange={(e) => setFormState(state => ({ ...state, budget: Number(e.target.value) }))} />
+                    </div>
+                    <div className="flex justify-end gap-5">
+                        <Button className="w-20" variant="outline" onClick={onAddProject}>Cancel</Button>
+                        <Button className="w-35"
+                            type="submit"
+                            disabled={isLoading}>
+                            {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+                            {isLoading ? "Adding..." : "Add Project"}
+                        </Button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    )
+}
+
+function ConnectIntegrations({ orgId }: { orgId: string }) {
     const [repoState, setRepoState] = useState<RepoState>({ repos: [], selectedRepo: null });
 
     useEffect(() => {
@@ -53,7 +130,6 @@ export default function AddProjectPage({ params }: { params: Promise<{ orgId: st
         getRepos();
     }, []);
 
-
     const openGithub = () => {
         const url = "https://github.com/apps/someorgapp/installations/new";
         openWindow(url);
@@ -64,82 +140,58 @@ export default function AddProjectPage({ params }: { params: Promise<{ orgId: st
         saveRepo(repo);
     }
 
-    const onAddProject = async () => {
-        try {
-            const supabase = createSupabaseBrowserClient();
-
-            const result = await supabase.functions.invoke("create-project", {
-                body: {
-                    org_public_id: orgId,
-                    proj_name: formState.projName,
-                    proj_desc: formState.projDesc,
-                    budget: formState.budget
-                }
-            });
-
-            if (result.error) throw result.error;
-            window.location.reload();
-
-        } catch (err: unknown) {
-            console.log(err instanceof Error ? err.message : err);
-        }
+    const onSkip = () => {
+        window.location.reload();
     }
 
+    const onDone = () => {
+        window.location.reload();
+    }
+
+
     return (
-        <div className="bg-slate-50 flex w-full justify-center">
-            <div className="m-8 mt-5 border-1 rounded-2xl bg-white max-w-3xl">
-                <div className="flex flex-col gap-10 mt-10 mx-10">
-                    <h1 className="text-2xl font-semibold">Add Project</h1>
-                    <div className="grid gap-3">
-                        <Label htmlFor="title-label">Name</Label>
-                        <Input type="text" id="title-label" aria-labelledby="title-label"
-                            onChange={(e) => setFormState(state => ({ ...state, projName: e.target.value }))} />
+        <div className="bg-slate-50 w-full flex justify-center">
+            <div className=" m-8 border-1 rounded-2xl bg-white sm:min-w-2xl max-w-3xl h-fit 
+            [&>*]:border-b-1 [&>*]:border-gray-200 [&>*]:px-8 [&>*]:py-6">
+                <div>
+                    <h2 className="text-2xl font-semibold">Connect Data Providers</h2>
+                    <p className="text-gray-500 text-sm">Integrate with issue tracking tools and code repositories</p>
+                </div>
+                <div className="grid gap-3">
+                    <Label>Connect Issue Tracking Tool</Label>
+                    <div className="grid grid-cols-2 sm:grid-cols-4 gap-2">
+                        <Button variant="secondary">Jira</Button>
+                        <Button variant="secondary">Azure Boards</Button>
                     </div>
-
-                    <div className="grid gap-3">
-                        <Label htmlFor="desc-label">Description</Label>
-                        <Textarea id="desc-label" aria-labelledby="desc-label"
-                            onChange={(e) => setFormState(state => ({ ...state, projDesc: e.target.value }))} />
-                    </div>
-
-                    <div className="grid gap-3">
-                        <Label htmlFor="budget-label">Budget</Label>
-                        <Input type="number" id="title-label" aria-labelledby="budget-label"
-                            onChange={(e) => setFormState(state => ({ ...state, budget: Number(e.target.value) }))} />
-                    </div>
-
-                    <div className="grid gap-3">
-                        <Label>Connect Issue Tracking Tool</Label>
-                        <div className="grid grid-cols-2 sm:grid-cols-4 gap-2">
-                            <Button variant="secondary">Jira</Button>
-                            <Button variant="secondary">Azure Boards</Button>
+                </div>
+                <div className="grid gap-3">
+                    <Label>Connect Repository</Label>
+                    <div className={`${repoState.repos.length ? "" : "hidden"}`}>
+                        <p className="text-sm text-gray-500 font-medium my-2">Available Repositories</p>
+                        <div className="border-1 rounded-lg p-3 flex flex-col divide-y">
+                            {repoState.repos.map(repo => (
+                                <div key={repo.id}
+                                    className="text-sm font-medium py-1 cursor-pointer"
+                                    onClick={() => onSelectRepository(repo)}>{repo.fullName}</div>
+                            ))}
                         </div>
                     </div>
-                    <div className="grid gap-3">
-                        <Label>Connect Repository</Label>
-                        <div className={`${repoState.repos.length ? "" : "hidden"}`}>
-                            <p className="text-sm text-gray-500 font-medium my-2">Available Repositories</p>
-                            <div className="border-1 rounded-lg p-3 flex flex-col divide-y">
-                                {repoState.repos.map(repo => (
-                                    <div key={repo.id}
-                                        className="text-sm font-medium py-1 cursor-pointer"
-                                        onClick={() => onSelectRepository(repo)}>{repo.fullName}</div>
-                                ))}
-
-                            </div>
-                        </div>
-                        <div className="grid grid-cols-2 sm:grid-cols-4 gap-2">
-                            <Button variant="secondary" onClick={openGithub}>GitHub (repo & issues)</Button>
-                            <Button variant="secondary">Bitbucket</Button>
-                            <Button variant="secondary">Azure Repos</Button>
-                        </div>
+                    <div className="grid grid-cols-2 sm:grid-cols-4 gap-2">
+                        <Button variant="secondary" onClick={openGithub}>GitHub (repo & issues)</Button>
+                        <Button variant="secondary">Bitbucket</Button>
+                        <Button variant="secondary">Azure Repos</Button>
                     </div>
-                    <Button className="w-50" onClick={onAddProject}>Add Project</Button>
+                </div>
+                <div className="flex justify-end gap-5">
+                    <Button className="w-30" variant="outline" onClick={onSkip}>Skip</Button>
+                    <Button className="w-35" type="submit" onClick={onDone}>Done</Button>
                 </div>
             </div>
         </div>
     )
 }
+
+
 
 
 function openWindow(url: string) {
